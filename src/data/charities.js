806b0fd62cca1e517f8f9438d5f930cc5b4cf02a
Dita Permata Putra <ditateum@gmail.com>
2022-01-27 18:36:43 +0700

    convert html template to svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://images.unsplash.com/photo-1526304640581-d334cdbbf45e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+            pledged: 99000,
+            target: 100000,
+            date_end: +new Date('10 August 2022'),
+            profile_photo: 'https://live.staticflickr.com/4027/4357722810_7136f4a9e9_s.jpg',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+        }
+    ]
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Header from "../components/Header.svelte";
+  import Welcome from "../components/Welcome.svelte";
+  import CharityList from "../components/CharityList.svelte";
+  import Promo from "../components/Promo.svelte";
+  import Footer from "../components/Footer.svelte";
+
+  let title = "Charity";
+</script>
+
+<Header />
+<Welcome />
+<CharityList />
+<Promo />
+<Footer />
